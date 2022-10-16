863cb3b5c62c996d0a1a6694b0ad9989550d433b
Ilnaz <gazimoviljasilxamovich@gmail.com>
2022-10-16 18:35:47 +0500

    nothing fixes

--- ApiShedule2022/HostedServices/NewSheduleHostedService.cs
+++ ApiShedule2022/HostedServices/NewSheduleHostedService.cs
@@ -25,170 +25,173 @@ namespace ApiShedule2022.HostedServices
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            try
+            while (!stoppingToken.IsCancellationRequested)
             {
-                XLWorkbook xl = null;
-                Differents dif = new Differents();
-                dif.DateOut(DateTime.UtcNow.AddHours(5).AddDays(7));
-                string trim1 = dif.upMonth.Substring(0, 3);
-                Differents diffe = new Differents();
-                diffe.DateOut(DateTime.UtcNow.AddHours(5));
-                if (!File.Exists(@$"{AppDomain.CurrentDomain.BaseDirectory}Raspisanie/_{diffe.downDay}_{diffe.upDay}_{trim1}.xlsx"))
+                try
                 {
-
-                }
-                else
-                {
-                    if (File.Exists(@$"{AppDomain.CurrentDomain.BaseDirectory}Raspisanie/_{dif.downDay}_{dif.upDay}_{trim1}.xlsx"))
+                    XLWorkbook xl = null;
+                    Differents dif = new Differents();
+                    dif.DateOut(DateTime.UtcNow.AddHours(5).AddDays(7));
+                    string trim1 = dif.upMonth.Substring(0, 3);
+                    Differents diffe = new Differents();
+                    diffe.DateOut(DateTime.UtcNow.AddHours(5));
+                    if (!File.Exists(@$"{AppDomain.CurrentDomain.BaseDirectory}Raspisanie/_{diffe.downDay}_{diffe.upDay}_{trim1}.xlsx"))
                     {
-                        xl = new XLWorkbook(@$"{AppDomain.CurrentDomain.BaseDirectory}Raspisanie/_{dif.downDay}_{dif.upDay}_{trim1}.xlsx");
-                        cache.Set("xLNew", xl.Worksheets.First(), new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5) });
+
                     }
                     else
                     {
-                        HtmlDocument doc = new HtmlDocument();
-                        var web1 = new HtmlWeb();
-                        doc = web1.Load("https://oksei.ru/studentu/raspisanie_uchebnykh_zanyatij");
-                        var node = doc.DocumentNode.SelectSingleNode("//*[@class='container bg-white p-25 box-shadow-right radius']/p/a");
-                        var href = node.Attributes["href"].Value;
-                        var value = node.InnerText;
-                        using (var stream = new StreamReader($"{AppDomain.CurrentDomain.BaseDirectory}Raspisanie/Formats.txt"))
+                        if (File.Exists(@$"{AppDomain.CurrentDomain.BaseDirectory}Raspisanie/_{dif.downDay}_{dif.upDay}_{trim1}.xlsx"))
                         {
-                            string l = stream.ReadToEnd();
-                            stream.Close();
-                            if (l != value)
+                            xl = new XLWorkbook(@$"{AppDomain.CurrentDomain.BaseDirectory}Raspisanie/_{dif.downDay}_{dif.upDay}_{trim1}.xlsx");
+                            cache.Set("xLNew", xl.Worksheets.First(), new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5) });
+                        }
+                        else
+                        {
+                            HtmlDocument doc = new HtmlDocument();
+                            var web1 = new HtmlWeb();
+                            doc = web1.Load("https://oksei.ru/studentu/raspisanie_uchebnykh_zanyatij");
+                            var node = doc.DocumentNode.SelectSingleNode("//*[@class='container bg-white p-25 box-shadow-right radius']/p/a");
+                            var href = node.Attributes["href"].Value;
+                            var value = node.InnerText;
+                            using (var stream = new StreamReader($"{AppDomain.CurrentDomain.BaseDirectory}Raspisanie/Formats.txt"))
                             {
-                                WebClient web = new WebClient();
-                                web.DownloadFile($"https://oksei.ru{href}", $"{AppDomain.CurrentDomain.BaseDirectory}Raspisanie/_{dif.DdownDay.Day}_{dif.DupDay.Day}_{dif.upMonth.Substring(0, 3)}.xls");
-                                var workbook = new Aspose.Cells.Workbook(@$"{AppDomain.CurrentDomain.BaseDirectory}Raspisanie/_{dif.downDay}_{dif.upDay}_{dif.upMonth.Substring(0, 3)}.xls");
-                                workbook.Save(@$"{AppDomain.CurrentDomain.BaseDirectory}Raspisanie/_{dif.downDay}_{dif.upDay}_{dif.upMonth.Substring(0, 3)}.xlsx", Aspose.Cells.SaveFormat.Xlsx);
-                                xl = new XLWorkbook(@$"{AppDomain.CurrentDomain.BaseDirectory}Raspisanie/_{dif.downDay}_{dif.upDay}_{dif.upMonth.Substring(0, 3)}.xlsx");
-                                cache.Set("xLNew", xl.Worksheets.First(), new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5) });
-                                using (var streame = new StreamWriter($"{AppDomain.CurrentDomain.BaseDirectory}Raspisanie/Formats.txt", false))
+                                string l = stream.ReadToEnd();
+                                stream.Close();
+                                if (l != value)
                                 {
-                                    streame.Write(value);
-                                    streame.Close();
+                                    WebClient web = new WebClient();
+                                    web.DownloadFile($"https://oksei.ru{href}", $"{AppDomain.CurrentDomain.BaseDirectory}Raspisanie/_{dif.DdownDay.Day}_{dif.DupDay.Day}_{dif.upMonth.Substring(0, 3)}.xls");
+                                    var workbook = new Aspose.Cells.Workbook(@$"{AppDomain.CurrentDomain.BaseDirectory}Raspisanie/_{dif.downDay}_{dif.upDay}_{dif.upMonth.Substring(0, 3)}.xls");
+                                    workbook.Save(@$"{AppDomain.CurrentDomain.BaseDirectory}Raspisanie/_{dif.downDay}_{dif.upDay}_{dif.upMonth.Substring(0, 3)}.xlsx", Aspose.Cells.SaveFormat.Xlsx);
+                                    xl = new XLWorkbook(@$"{AppDomain.CurrentDomain.BaseDirectory}Raspisanie/_{dif.downDay}_{dif.upDay}_{dif.upMonth.Substring(0, 3)}.xlsx");
+                                    cache.Set("xLNew", xl.Worksheets.First(), new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5) });
+                                    using (var streame = new StreamWriter($"{AppDomain.CurrentDomain.BaseDirectory}Raspisanie/Formats.txt", false))
+                                    {
+                                        streame.Write(value);
+                                        streame.Close();
+                                    }
                                 }
                             }
                         }
-                    }
-                    if (xl is not null)
-                    {
-                        var workSheet = xl.Worksheets.First();
-                        await Task.Run(() =>
+                        if (xl is not null)
                         {
-                            bool exit = true;
-                            List<string> dataforcb = new List<string>();
-                            for (int i = 3; i <= workSheet.ColumnsUsed().Count(); i++)
+                            var workSheet = xl.Worksheets.First();
+                            await Task.Run(() =>
                             {
-                                for (int j = 6; j <= workSheet.RowsUsed().Count(); j++)
+                                bool exit = true;
+                                List<string> dataforcb = new List<string>();
+                                for (int i = 3; i <= workSheet.ColumnsUsed().Count(); i++)
                                 {
-                                    string result = workSheet.Cell(j, i).GetValue<string>();
-                                    if (result != "" && result != " " && result.Length > 3)
+                                    for (int j = 6; j <= workSheet.RowsUsed().Count(); j++)
                                     {
-                                        result = result.Remove(0, result.Length - 3).Trim();
-                                        if (result != "-" && result != "" && result.Length > 1)
+                                        string result = workSheet.Cell(j, i).GetValue<string>();
+                                        if (result != "" && result != " " && result.Length > 3)
                                         {
-                                            Regex regex = new Regex("[0-9]{2,3}[^)]");
-                                            if (regex.IsMatch(result))
+                                            result = result.Remove(0, result.Length - 3).Trim();
+                                            if (result != "-" && result != "" && result.Length > 1)
                                             {
-                                                foreach (string output in dataforcb)
+                                                Regex regex = new Regex("[0-9]{2,3}[^)]");
+                                                if (regex.IsMatch(result))
                                                 {
-                                                    if (output == result)
+                                                    foreach (string output in dataforcb)
                                                     {
-                                                        exit = false;
-                                                        break;
+                                                        if (output == result)
+                                                        {
+                                                            exit = false;
+                                                            break;
+                                                        }
                                                     }
+                                                    if (exit)
+                                                    {
+                                                        dataforcb.Add(result);
+                                                    }
+                                                    exit = true;
                                                 }
-                                                if (exit)
-                                                {
-                                                    dataforcb.Add(result);
-                                                }
-                                                exit = true;
                                             }
                                         }
                                     }
                                 }
-                            }
-                            dataforcb.Sort();
-                            cache.Set("NewListCabinets", dataforcb, new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5) });
-                        }); //список кабинетов
-                        await Task.Run(() =>
-                        {
-                            bool exit = true;
-                            List<string> dataforcb = new List<string>();
-                            for (int i = 3; i <= workSheet.ColumnsUsed().Count(); i++)
+                                dataforcb.Sort();
+                                cache.Set("NewListCabinets", dataforcb, new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5) });
+                            }); //список кабинетов
+                            await Task.Run(() =>
                             {
-                                for (int j = 6; j <= workSheet.RowsUsed().Count(); j++)
+                                bool exit = true;
+                                List<string> dataforcb = new List<string>();
+                                for (int i = 3; i <= workSheet.ColumnsUsed().Count(); i++)
                                 {
-                                    string result = workSheet.Cell(j, i).GetValue<string>();
-                                    if (result != "" && result != " " && result.Length > 3)
+                                    for (int j = 6; j <= workSheet.RowsUsed().Count(); j++)
                                     {
-                                        if (result.Contains("ДОП"))
+                                        string result = workSheet.Cell(j, i).GetValue<string>();
+                                        if (result != "" && result != " " && result.Length > 3)
                                         {
-                                            string[] massiv = result.Split(new char[] { '(', ')' });
-                                            result = massiv[1].Trim();
-                                        }
-                                        else
-                                        {
-                                            try
+                                            if (result.Contains("ДОП"))
                                             {
-                                                if (result.Length == 4)
-                                                {
-                                                    continue;
-                                                }
-                                                string[] massiv = result.Split('\n');
-                                                if (massiv.Length == 1)
-                                                { continue; }
+                                                string[] massiv = result.Split(new char[] { '(', ')' });
                                                 result = massiv[1].Trim();
                                             }
-                                            catch
-                                            { continue; }
-                                        }
-                                        if (result != "-" && result != "")
-                                        {
-                                            Regex regex = new Regex(@"[а-яА-Я]+\s[А-Я]{1}\.[А-Я]{1}\.?$");
-                                            if (regex.IsMatch(result))
+                                            else
                                             {
-                                                foreach (string output in dataforcb)
+                                                try
                                                 {
-                                                    if (output == result)
+                                                    if (result.Length == 4)
                                                     {
-                                                        exit = false;
-                                                        break;
+                                                        continue;
                                                     }
+                                                    string[] massiv = result.Split('\n');
+                                                    if (massiv.Length == 1)
+                                                    { continue; }
+                                                    result = massiv[1].Trim();
                                                 }
-                                                if (exit)
+                                                catch
+                                                { continue; }
+                                            }
+                                            if (result != "-" && result != "")
+                                            {
+                                                Regex regex = new Regex(@"[а-яА-Я]+\s[А-Я]{1}\.[А-Я]{1}\.?$");
+                                                if (regex.IsMatch(result))
                                                 {
-                                                    dataforcb.Add(result);
+                                                    foreach (string output in dataforcb)
+                                                    {
+                                                        if (output == result)
+                                                        {
+                                                            exit = false;
+                                                            break;
+                                                        }
+                                                    }
+                                                    if (exit)
+                                                    {
+                                                        dataforcb.Add(result);
+                                                    }
+                                                    exit = true;
                                                 }
-                                                exit = true;
                                             }
                                         }
                                     }
                                 }
-                            }
-                            dataforcb.Sort();
-                            cache.Set("NewListTeachers", dataforcb, new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5) });
-                        });
-                        await Task.Run(() =>
-                        {
-                            List<string> dataforcb = new List<string>();
-                            for (int i = 3; i <= workSheet.ColumnsUsed().Count(); i++)
+                                dataforcb.Sort();
+                                cache.Set("NewListTeachers", dataforcb, new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5) });
+                            });
+                            await Task.Run(() =>
                             {
-                                dataforcb.Add(workSheet.Cell(5, i).GetValue<string>());
-                            }
-                            dataforcb.Sort();
-                            cache.Set("NewListGroups", dataforcb, new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5) });
-                        });
+                                List<string> dataforcb = new List<string>();
+                                for (int i = 3; i <= workSheet.ColumnsUsed().Count(); i++)
+                                {
+                                    dataforcb.Add(workSheet.Cell(5, i).GetValue<string>());
+                                }
+                                dataforcb.Sort();
+                                cache.Set("NewListGroups", dataforcb, new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5) });
+                            });
+                        }
                     }
                 }
-            }
-            catch
-            {
+                catch
+                {
 
+                }
+                await Task.Delay(1000 * 60 * 5);
             }
-            await Task.Delay(1000 * 60 * 5);
         }
     }
 }
